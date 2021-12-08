8104f4e279675ce2c0b06f006a81dd4b64c4ae99
JordanAWatson <jordanawatson@hotmail.com>
2021-12-08 00:45:50 -0800

    reverted to working version

--- LLSPI_Tracker.py
+++ LLSPI_Tracker.py
@@ -2,6 +2,7 @@ import PySimpleGUI as sg
 import configparser
 
 data_file = "data.ini"
+num_chapters = 10
 
 config = configparser.ConfigParser()
 config.read(data_file)
@@ -13,7 +14,7 @@ has_FS_default = config.getboolean("OWNED", "FS")
 
 track_keys = []
 x = 0
-for chapter in range(1, 36):
+for chapter in range(1, num_chapters):
     for checkbox in range(1, 4):
         track_keys.append(config.getboolean("TRACK", "c" + str(chapter) + "_" + str(checkbox)))
         x = x + 1
@@ -26,14 +27,15 @@ layout = [[sg.Checkbox("FR", default=has_FR_default, key="has_FR"), sg.Text("Fam
           [sg.Checkbox("FS", default=has_FS_default, key="has_FS"), sg.Text("Fabulae Syrae")],
           [sg.HorizontalSeparator()]]
 
-for i in range(1, 103):
-    print(str(int(i / 3) + 1) + "_1")
-    print(str(int(i / 3) + 1) + "_2")
-    print(str(int(i / 3) + 1) + "_3")
-    layout.append([sg.Checkbox("", default=track_keys[int(i / 3) + 1], key=(str(i) + "_1")),
-                   sg.Checkbox("", default=track_keys[int(i / 3) + 1], key=(str(i) + "_2")),
-                   sg.Checkbox("", default=track_keys[int(i / 3) + 1], key=(str(i) + "_3")), sg.Text(i)])
-    i += 3
+# FIXME something here makes previous checkboxes return None
+#for i in range(1, num_chapters * 3):
+#    print(str(int(i / 3) + 1) + "_1")
+#    print(str(int(i / 3) + 1) + "_2")
+#    print(str(int(i / 3) + 1) + "_3")
+#    layout.append([sg.Checkbox("", default=track_keys[int(i / 3) + 1], key=(str(i) + "_1")),
+#                   sg.Checkbox("", default=track_keys[int(i / 3) + 1], key=(str(i) + "_2")),
+#                   sg.Checkbox("", default=track_keys[int(i / 3) + 1], key=(str(i) + "_3")), sg.Text(i)])
+#    i += 3
 
 layout.append([sg.Button("Exit")])
 
@@ -43,7 +45,7 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Exit":
         break
-
+print(values)
 config["OWNED"]["FR"] = str(values["has_FR"])
 config["OWNED"]["FL"] = str(values["has_FL"])
 config["OWNED"]["CP"] = str(values["has_CP"])
